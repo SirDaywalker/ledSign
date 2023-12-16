3fac4ff8549f8ee26360c1504446a52dcb2e2915
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2023-12-16 10:18:33 +0100

    added http mapping by microdot

--- main.py
+++ main.py
@@ -2,49 +2,100 @@ __author__ = "Jannis Dickel"
 
 from lib.leds import Leds
 from lib.usSensor import UsSensor
-# from lib.server import Server
-from time import sleep
+from lib.microdot_asyncio import Microdot
+
+import uasyncio as asyncio
+
+led: Leds = Leds(6, 20)
+us_sensor: UsSensor = UsSensor(16, 17)
+app: Microdot = Microdot()
+current_task = None
 
 
-def main() -> None:   
-    led: Leds = Leds(6, 20)
-    us_sensor: UsSensor = UsSensor(16, 17)
-    # server = Server(80)
-    # server.start()
-    # server.run()
+def start_server():
+    print("Starting webserver...")
+    try:
+        app.run(port=80)
+    except:
+        app.shutdown()
 
-    led.set_all(led.OFF)
 
-    while True:
-        distance: int = int(us_sensor.read_distance())
-        
-        if distance < 3:
-            led.blink_up()
-            led.fade(led.OFF)
-           
-        elif 3 < distance < 6:
-            led.fade(led.RED)
-            
-        elif 6 < distance < 9:
-            led.fade(led.YELLOW)
-            
-        elif 9 < distance < 12:
-            led.fade(led.GREEN)
-            
-        elif 12 < distance < 15:
-            led.fade(led.CYAN)
-            
-        elif 15 < distance < 18:
-            led.fade(led.BLUE)
-            
-        elif 18 < distance < 21:
-            led.fade(led.PURPLE)
-            
-        else:
-            pass
-        
-        sleep(0.5)
+@app.before_request
+async def kill_all_tasks(request):
+    if current_task:
+        current_task.cancel()
 
-    
-if __name__ == '__main__':
-    main()
+
+@app.route('/')
+def hello(request) -> (str, int):
+    return "Raspberry Pi Pico W Http Web Server", 200
+
+
+@app.route("/set-rgb-color")
+def fade_color(request) -> (str, int):
+    # http://<ip addr>/change-color?r=[int]&g=[int]&b=[int]
+    led.set_all((
+        int(request.args['r']),
+        int(request.args['g']),
+        int(request.args['b'])
+    ))
+    return "Changed color", 200
+
+
+@app.route("/set-hsv-color")
+def fade_color(request) -> (str, int):
+    # http://<ip addr>/change-color?h=[int]&s=[int]&v=[int]
+    led.set_all((
+        led.convert_hsv_to_rgb(
+            int(request.args['h']),
+            int(request.args['s']),
+            int(request.args['v'])
+        )
+    ))
+    return "Changed color", 200
+
+
+@app.route("/fade-rgb-color")
+def fade_color(request) -> (str, int):
+    # http://<ip addr>/change-color?r=[int]&g=[int]&b=[int]
+    led.fade((
+        int(request.args['r']),
+        int(request.args['g']),
+        int(request.args['b'])
+    ))
+    return "Changed color", 200
+
+
+@app.route("/fade-hsv-color")
+def fade_color(request) -> (str, int):
+    # http://<ip addr>/change-color?h=[int]&s=[int]&v=[int]
+    led.fade((
+        led.convert_hsv_to_rgb(
+            int(request.args['h']),
+            int(request.args['s']),
+            int(request.args['v'])
+        )
+    ))
+    return "Changed color", 200
+
+
+@app.route("/candy-tornado")
+def candy_tornado(request) -> (str, int):
+    # http://<ip addr>/candy-tornado?
+    # sat=[int]&val=[int]&delay_ms=[int]&hue_gap=[int]&hue_cycle_speed=[int]
+    # <all path variables optional>
+
+    args_dict = {}
+    for key in request.args.keys():
+        args_dict[key] = int(request.args[key])
+
+    global current_task
+    try:
+        current_task = asyncio.create_task(led.candy_tornado(**args_dict))
+    except TypeError:
+        return "Non fitting params", 400
+
+    return "Going wild", 200
+
+
+start_server()
