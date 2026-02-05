89a79310103b14fbddd592e934a0e16f134abe2d
Leon Reusch <leon@reusch-home.de>
2026-02-05 20:41:54 +0100

    feat: add StartColor setting

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ current_task: asyncio.Task = None
 try:
     if wifi.status() == 3:
         led.blink_up(led.GREEN)
+        led.fade(SETTINGS["StartColor"])
 except Exception:
     pass 
 
--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
+from lib.leds import Leds
+
 SETTINGS: dict = {
     # WIFI
     "SSID": "Your WIFI Name",
     "Password": "Your WIFI password",
 
     # LEDs
-    "AnzLEDs": 6,  # Number of LEDs
+    "NumLEDs": 6,  # Number of LEDs
     "LEDPin": 28,  # The pin your LEDs are connected to
+    "StartColor": Leds.OFF,
 
     # UsSensor
     "TriggerPin": 16,  # The pin of the trigger of your ultrasound sensor
