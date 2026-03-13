11d1a698824050e347a44bc30cd1f7614bfaaf90
Leon4ever2802 <94672224+Leon4ever2802@users.noreply.github.com>
2026-03-13 12:42:09 +0100

    feat: add wifi connection threat (#14)
    When the WiFi is not available while booting, a WiFi connection thread will be started, which continously tries to connect to the WiFi.

--- main.py
+++ main.py
@@ -12,7 +12,34 @@ from settings import SETTINGS
 led: Leds = Leds(SETTINGS["NumLEDs"], SETTINGS["LEDPin"])
 us_sensor: UsSensor = UsSensor(SETTINGS["TriggerPin"], SETTINGS["EchoPin"])
 app: Microdot = Microdot()
-current_task: asyncio.Task = None 
+current_task: asyncio.Task = None
+
+
+async def check_connection() -> bool:
+    counter = 0
+    while counter != 5:
+        if global_wifi.status() == 3:
+            return True
+        await asyncio.sleep(1)
+        counter += 1
+    return False
+
+
+async def connect_to_wifi() -> None:
+    """
+    Function which continuously tries to connect to the Wi-Fi.
+    Distance measuring with the us-sensor will still work and change the LEDs.
+
+    :return: None
+    """
+    while True:
+        global_wifi.connect(SETTINGS["SSID"], SETTINGS["Password"])
+
+        if await check_connection():
+            print(f"\033[92mConnected successfully to Wi-Fi! As: {global_wifi.ifconfig()[0]}\033[0m")
+            break
+
+        global_wifi.disconnect()
 
 # This is still part of the Wi-Fi connection
 try:
@@ -20,6 +47,8 @@ try:
         led.blink_up(target_color=led.GREEN)
     else:
         led.blink_up(sleep_time=0.2)
+        asyncio.create_task(connect_to_wifi())
+        print("  Trying to connect to Wi-Fi continuously!")
     if "StartColor" in SETTINGS:
         led.fade(SETTINGS["StartColor"])
 except Exception as e:
